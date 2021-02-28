9dad66f8cd37fe4919287a35a635d0225df92dbf
Xu Mingyu <592882665@qq.com>
2021-02-28 19:46:27 +0800

    Repo Initialization

---
+++ HousePrice/PredictHousePrice.py
@@ -0,0 +1,128 @@
+# In[]
+import torch
+from torch import nn
+from torch.utils import data
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_data = pd.read_csv("../../Datasets/HousePrice/train.csv")
+test_data = pd.read_csv("../../Datasets/HousePrice/test.csv")
+print(train_data.shape)
+print(test_data.shape)
+
+# In[]
+# feature preprocessing
+all_features = pd.concat((train_data.iloc[:,1:-1], test_data.iloc[:,1:-1]))
+
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(
+    lambda x: (x - x.mean()) / (x.std()))
+# After standardizing the data all means vanish, hence we can set missing
+# values to 0
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+all_features = pd.get_dummies(all_features, dummy_na=True)
+
+n_train = train_data.shape[0]
+train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float32)
+test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float32)
+train_labels = torch.tensor(
+    train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32)
+
+n_features = train_features.shape[1]
+loss = nn.MSELoss()
+
+
+def get_net():
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.normal_(m.weight, std=0.01)
+    net = nn.Sequential(nn.Linear(n_features,1))
+    net.apply(init_weights)
+    return net
+
+def log_rmse(net, features, labels):
+    # To further stabilize the value when the logarithm is taken, set the
+    # value less than 1 as 1
+    clipped_preds = torch.clamp(net(features), 1, float('inf'))
+    rmse = torch.sqrt(loss(torch.log(clipped_preds),
+                           torch.log(labels)))
+    return rmse.item()
+
+def load_data(train_data, batch_size):
+    dataset = data.TensorDataset(*train_data)
+    return data.DataLoader(dataset, batch_size, shuffle=True)
+    
+    
+def train(net, train_features, train_labels, test_features, test_labels,
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    train_iter = load_data((train_features, train_labels), batch_size)
+    # The Adam optimization algorithm is used here
+    optimizer = torch.optim.Adam(net.parameters(),
+                                 lr = learning_rate,
+                                 weight_decay = weight_decay)
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            optimizer.zero_grad()
+            l = loss(net(X), y)
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+# In[] K-Fold Cross-Validation
+def get_k_fold_data(k, i, X, y):
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat([X_train, X_part], 0)
+            y_train = torch.cat([y_train, y_part], 0)
+    return X_train, y_train, X_valid, y_valid
+
+def k_fold(k, X_train, y_train, num_epochs, learning_rate, weight_decay,
+           batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net()
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate,
+                                   weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        print(f'fold {i + 1}, train log rmse {float(train_ls[-1]):f}, '
+              f'valid log rmse {float(valid_ls[-1]):f}')
+    return train_l_sum / k, valid_l_sum / k
+
+k, num_epochs, lr, weight_decay, batch_size = 5, 100, 5, 0, 64
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr,
+                          weight_decay, batch_size)
+print(f'{k}-fold validation: avg train log rmse: {float(train_l):f}, '
+      f'avg valid log rmse: {float(valid_l):f}')
+
+# In[] Train and Predict
+def train_and_pred(train_features, test_feature, train_labels, test_data,
+                   num_epochs, lr, weight_decay, batch_size):
+    net = get_net()
+    train_ls, _ = train(net, train_features, train_labels, None, None,
+                        num_epochs, lr, weight_decay, batch_size)
+    print(f'train log rmse {float(train_ls[-1]):f}')
+    # Apply the network to the test set
+    preds = net(test_features).detach().numpy()
+    # Reformat it to export to Kaggle
+    test_data['SalePrice'] = pd.Series(preds.flatten())
+    submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    submission.to_csv('submission.csv', index=False)
+    
+train_and_pred(train_features, test_features, train_labels, test_data,
+               num_epochs, lr, weight_decay, batch_size)
